71eae39afa26775ca8c2ac76fa345a992097c4f9
Madhav Jindal <64203410+Madhav-Jindal@users.noreply.github.com>
2020-08-11 01:46:18 +0530

    Updated for put call as well

--- Quant_Assign_Q1.py
+++ Quant_Assign_Q1.py
@@ -1,9 +1,10 @@
 import numpy as np
+import matplotlib.pyplot as plt 
 
 
 # Function for option value calculation
 
-def bapm(S0,vol,N,r,K,T,):
+def bapm(S0,vol,N,r,K,T,call='C'):
 
     """
         S0 - initial stock price
@@ -12,7 +13,9 @@ def bapm(S0,vol,N,r,K,T,):
         r - risk-free interest rate
         K - strike price of the option
         T - time until expiry of the option
+        call - Add P for put , by default set for call
     """
+    
     dt=T/N 
     # u=np.exp(vol*np.sqrt(dt))
     # v=1/u
@@ -33,14 +36,28 @@ def bapm(S0,vol,N,r,K,T,):
 
 # Option Value tree 
     option=np.zeros([N+1,N+1])
-    option[:,N]=np.maximum(np.zeros(N+1),(asset[:,N]-K))
+    #Final node calculation
+    if call=='C':
+        option[:,N]=np.maximum(np.zeros(N+1),(asset[:,N]-K))
+    else:
+        option[:,N]=np.maximum(np.zeros(N+1),(K-asset[:,N]))
+    
+
     for i in range(N-1,-1,-1):    
         for j in range(0,i+1):
-            option[j,i]=max(0,asset[i,j]-K,np.exp(-r*dt)*((p*option[j,i+1])+((1-p)*option[j+1,i+1])))# Modified for American verison
+            # Modified for American verison
+             if call=='C':
+                option[j,i]=max(0,asset[i,j]-K,np.exp(-r*dt)*((p*option[j,i+1])+((1-p)*option[j+1,i+1])))
+             else:
+                 option[j,i]=max(0,K-asset[i,j],np.exp(-r*dt)*((p*option[j,i+1])+((1-p)*option[j+1,i+1])))
+    
+    
             
     return(option[0,0])
 
     
-
-
 print("Option value is :{0:0.4}".format(bapm(100,0.2,4,0.1,100,0.33)))
+#Modify here
+# T to be entered in years
+    # for ex-
+    # 4 months is 1/3 years or 0.33 years
